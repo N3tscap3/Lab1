37737f1b541d52de86e76949f0bb7cd2c345f312
Zamaraev Arseny <ars.zamaraev@gmail.com>
2021-12-08 21:49:15 +0300

    added  class Cheese

---
+++ src/bsu/rfe/java/group6/lab1/ZAMARAEV/var13A/Cheese.java
@@ -0,0 +1,11 @@
+package bsu.rfe.java.group6.lab1.ZAMARAEV.var13A;
+
+public class Cheese extends Food {
+    public Cheese() {
+        super("Сыр");
+    }
+
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+}
